1187886557b19390891845bca9fae50ebd1e176d
MTSBeta <taku@metimestories.co.uk>
2026-07-02 02:53:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.post("/chat", async (req, res) => {
 
 // ✅ Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
